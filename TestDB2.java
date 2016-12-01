8efba46ed0c7692bd926ccbbd44b0706af6ad85a
nikosgalanos <t8150019@dias.aueb.gr>
2016-12-01 16:26:12 +0200

    Update of connection to a database
    Can also now insert data in database.
    Class now organized in some methods

--- TestDB2.java
+++ TestDB2.java
@@ -1,51 +1,88 @@
-/* First attempt to connect with a database created on MS SQL Management Studio
+/* Second (successful) attempt to connect with a database created on MS SQL Management Studio
  * Purpose of this class: Get data saved in my database
  * Database Login-data are hidden on purpose
+ * now can also insert data in database as well as it can (Select - From- Where) them.
  */
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
-public class TestDB2 {
+public class DataBaseConn {
 	
-	public static void main(String[] args) {
-		
-		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			System.out.println("Driver Loaded");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		final String DATABASE_URL = "jdbc:sqlserver://@@@@@@";  // instead of @@@@@ use path of the database
-		final String SELECT_QUERY = "SELECT * FROM ex1_Customer";
+	//fields
+	
+	private static String link = "karxies";
+	private static String path = "laika korasia";
 
-		try (
-			Connection connection = DriverManager.getConnection(DATABASE_URL, "@@@", "@@@"); // instead of @@@ use username, password
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(SELECT_QUERY); )
+	// setters and getters
+	
+	public String getLink() {
+		return link;
+	}
 
-		{
-			ResultSetMetaData metaData = resultSet.getMetaData();
-			int numberOfColumns = metaData.getColumnCount();
-			System.out.printf("Customers Table of Test1 Database:%n%n");
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}	
+	
+	public static void main(final String[] args) throws SQLException {
+	
+	//loads driver of sqlDB and prints success or failure message!!
+	
+	try {
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		System.out.println("Driver Loaded");
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	
+	//DataBase login data
+	
+	final String DataBase_Path = "@@@";
+	final String DataBase_Username = "@@";
+	final String DataBase_PassWord = "@@";
+	
+	// insert data into table
+	
+	Connection connection = DriverManager.getConnection(DataBase_Path, DataBase_Username, DataBase_Password);
+	Statement statement = connection.createStatement();
+	statement.executeUpdate("INSERT INTO Result VALUES('" + link + "' , '" + path + "')"); 
+	
+	// fields
+		String Select_Query = "SELECT * FROM Result";
+	
+	//get data from table
+	
+	try (ResultSet resultSet = statement.executeQuery(Select_Query);
+		)
+
+	{
+		 ResultSetMetaData metaData = resultSet.getMetaData();
+		int numberOfColumns = metaData.getColumnCount();
+		System.out.printf("Customers Table of Test1 Database:%n%n");
+		for (int i = 1; i <= numberOfColumns; i++)
+			System.out.printf("%-45s\t", metaData.getColumnName(i));
+		System.out.println();
+		while (resultSet.next()) {
 			for (int i = 1; i <= numberOfColumns; i++)
-				System.out.printf("%-8s\t",  metaData.getColumnName(i));
+				System.out.printf("%-45s\t", resultSet.getObject(i));
 			System.out.println();
-			while (resultSet.next())
-			{
-				for (int i = 1; i <= numberOfColumns; i++)
-					System.out.printf("%-8s\t", resultSet.getObject(i));
-				System.out.println();
 			}
-		}
-		catch (SQLException sqlException)
-		{
-			sqlException.printStackTrace();
-		}
+	} catch (final SQLException sqlException) {
+		sqlException.printStackTrace();
 	}
 }
+}
+
