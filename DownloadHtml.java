5fc91efd7cd48987115dc549db3ee5b55792bb91
Vaggelis-Malandrakis <vagmalandrakis@yahoo.gr>
2016-12-02 00:19:29 +0200

    Αρχείο που κατεβάζει τα αρχεία html και τα αποθηκεύει σε φακέλους
    Η κλάση λαμβάνει ως όρισμα ένα αντικείμενο τύπου URL. Στη κεντρική
    μέθοδο δημιουργεί (αν δεν υπάρχει ήδη) τον φάκελο που θα αποθηεκευτεί το
    αρχείο και στη συνέχεια παράγει ένα τυχαίο όνομα αρχείου. Και στις δύο
    περιπτώσεις ελέγχει αν το όνομα του φακέλου ή του αρχείου υπάρχει ήδη
    (δημιουργεί δηλαδή μοναδικά αρχεία). Στη συνέχεια χρησιμοποιώντας
    κωδικοποίηση UTF-8 διαβάζει γραμμή - γραμμή τον htnl κώδικα και
    παράλληλα τον αποθηκεύει στο αρχείο που έχει δημιουργήσει προηγουμένως.

---
+++ DownloadHtml.java
@@ -0,0 +1,134 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.Random;
+
+public class DownloadHtml {
+	private URL url;
+	private static final char[] ab = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+									  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	
+	public DownloadHtml(URL url) {
+		this.url = url;
+	}
+	
+	public String parseAndSaveHtml () {
+		try {	
+			String inputLine;
+			
+			// creates the folder's name from the domain name
+			String folderName = nameFolder();	
+			File folderNameFile = new File("C:\\Users\\Vaggelis\\Desktop\\DownloadHtml\\"+folderName);
+			if (!existsFolder(folderNameFile)) {
+				makeFolder(folderNameFile);
+			}
+			
+			// creates the file with a random unique name
+			String fileName = generateFileName(folderName);
+			File file = new File("C:\\Users\\Vaggelis\\Desktop\\DownloadHtml\\" + folderName + "\\" + fileName + ".txt");
+			
+			// streams which help to read from the url
+			InputStreamReader is = new InputStreamReader(url.openStream(), "UTF-8");
+			BufferedReader br = new BufferedReader(is);
+			
+			// streams which help to write from the url
+			FileOutputStream fos = new FileOutputStream(file);
+			OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+			BufferedWriter bw = new BufferedWriter(osw);
+			
+			// reads line by line the html code
+			while ((inputLine = br.readLine()) != null) {
+				bw.write(inputLine);
+				bw.newLine();
+			}
+			bw.close(); //closes the writer stream
+			
+			String toReturn = "Html page: " + url.toString() + " saved as: " + fileName + " in folder: " + folderName;
+			return toReturn;
+		}
+		catch (UnsupportedEncodingException e) {
+			System.err.println("Error: Unsupported Encoding. Please try again.");
+			System.exit(1);
+		}
+		catch (FileNotFoundException e) {
+			System.err.println("Error: File Not Found. Please try again.");
+			System.exit(1);
+		}
+		catch (IOException e) {
+			System.err.println("Error: Input or Output Exception. Please try again.");
+			System.exit(1);
+		}
+		return null; //in case of an exception;
+	}
+	
+	// returns the domain name so as to name the folder
+	public String nameFolder() {
+		String stringUrl = urlToString();
+		int position1 = stringUrl.indexOf("www."); 					    // position of first "w"
+		int position2; 						 					        // position before the country's suffix
+		
+		// case link is something "http://listofrandomwords.com/"
+		if (position1 == -1) {
+			position1 = stringUrl.indexOf("://"); 					    // "://" is after http and before domain name
+			position2 = stringUrl.indexOf(".", 6);
+			return stringUrl.substring(position1 + 3, position2);		// position + 3 = first letter of domain
+		} else {
+			position2 = stringUrl.indexOf(".", 11);
+			return stringUrl.substring(position1 + 4, position2);		// position + 4 = first letter of domain
+		}
+	}
+	
+	// converts Url to String
+	public String urlToString() {
+		return url.toString();
+	}
+	
+	// returns true if the folder has already been created
+	public boolean existsFolder(File folderNameFile) {
+		if (folderNameFile.exists()) {
+			return true;
+		}
+		return false;
+	}
+	
+	// creates a directory with the given name
+	public void makeFolder (File folderNameFile) {
+		folderNameFile.mkdir();
+	}
+	
+	// produces the file name which has this format x000000000
+	// x: random letter | 0: random digit
+	public String generateFileName (String folderName) {
+		StringBuilder sb = new StringBuilder(10);
+		String fileName;
+		for (;;) {
+			Random ran1 = new Random();
+			sb.append(ran1.nextInt(9));
+			for (int i = 0; i < 9; i++) {
+				Random ran2 = new Random();
+				sb.append(ab[ran2.nextInt(26)]);
+			}
+			fileName = sb.toString();
+			File testFile = new File("C:\\Users\\Vaggelis\\Desktop\\DownloadHtml\\" + folderName + "\\" + fileName + ".txt");
+			if (!existsFile(testFile)) {
+				break;
+			}
+		}
+		return fileName;
+	}
+	
+	// returns true if there is a file with the same name in the directory
+	public boolean existsFile (File testFile) {
+		if (testFile.exists()) {
+			return true;
+		}
+		return false;
+	}
+}
