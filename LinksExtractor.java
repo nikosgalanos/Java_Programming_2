dde7e7a5171692151c0febde38cbf2603d04a57c
chiotisn <chiotisn@yahoo.com>
2016-12-07 22:10:39 +0200

    LinksExtractor Updated
    1. IOException handled in getLinks() method.
    2. Arraylist replaced with HashSet for more efficiency.
    3. An useless method deleted.

--- LinksExtractor.java
+++ LinksExtractor.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.lang.StringBuilder;
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 /**A class that extracts links from an URL*/
 public class LinksExtractor {
@@ -15,6 +16,7 @@ public class LinksExtractor {
 	/**Class constructor that takes an URL as an argument*/
 	public LinksExtractor(URL x) {
 		url = x;
+		urlString = url.toString();
 	}
 
 	/**Class constructor that takes a String as an argument and creates an URL*/
@@ -41,41 +43,46 @@ public class LinksExtractor {
 		return builder.toString();
 	}
 
-	/**A method that returns an ArrayList with the links of HTML code*/ 
-	public ArrayList<String> getLinks() throws IOException {
-		ArrayList<String> links = new ArrayList<String>();
+	/**A method that returns an HashSet with the links of HTML code*/ 
+	public HashSet<String> getLinks() {
+		HashSet<String> links = new HashSet<String>();
 		BufferedReader buffer = null;
-		buffer = new BufferedReader(new InputStreamReader(url.openStream()));
-		char[] last4 = new char[5];
-		boolean flag = true;
-		int ch;
-		while ((ch = buffer.read()) != -1) {
-			if (flag) {
-				last4[1] = (char) ch;
-				last4[2] = (char) buffer.read();
-				last4[3] = (char) buffer.read();
-				last4[4] = (char) buffer.read();
-				flag = false;
-			} else {
-				last4[1] = last4[2];
-				last4[2] = last4[3];
-				last4[3] = last4[4];
-				last4[4] = (char) ch;
-			}
-			if ( last4[1] == 'h' && last4[2] == 'r' && last4[3] == 'e' && last4[4] == 'f') {
-				StringBuilder link = new StringBuilder();
-				buffer.read();
-				buffer.read();
-				while ((ch = buffer.read()) != '"') {
-					link.append((char) ch);
+		try {
+			buffer = new BufferedReader(new InputStreamReader(url.openStream()));
+			char[] last4 = new char[5];
+			boolean flag = true;
+			int ch;
+			while ((ch = buffer.read()) != -1) {
+				if (flag) {
+					last4[1] = (char) ch;
+					last4[2] = (char) buffer.read();
+					last4[3] = (char) buffer.read();
+					last4[4] = (char) buffer.read();
+					flag = false;
+				} else {
+					last4[1] = last4[2];
+					last4[2] = last4[3];
+					last4[3] = last4[4];
+					last4[4] = (char) ch;
 				}
-				//correct Url format
-				if (UrlCheck.CorrectUrl(urlString, link.toString()) != "-1") {
-					links.add(UrlCheck.CorrectUrl(urlString, link.toString()));
+				if ( last4[1] == 'h' && last4[2] == 'r' && last4[3] == 'e' && last4[4] == 'f') {
+					StringBuilder link = new StringBuilder();
+					buffer.read();
+					buffer.read();
+					while ((ch = buffer.read()) != '"') {
+						link.append((char) ch);
+					}
+					//correct Url format
+					if (UrlCheck.CorrectUrl(urlString, link.toString()) != "-1") {
+						links.add(UrlCheck.CorrectUrl(urlString, link.toString()));
+					}
 				}
 			}
+			buffer.close();
+		} catch (IOException e) {
+			System.out.println("1 exception caught and handled in LinksExtractor class");
+			return null;
 		}
-		buffer.close();
 		return links;
 	}
 }
