1914f1dc8e5fcd6845c9d6d8bb521ce07664a707
chiotisn <chiotisn@yahoo.com>
2016-12-07 22:54:36 +0200

    UrlCrawler Updated
    The connection with the other parts was done

--- UrlCrawler.java
+++ UrlCrawler.java
@@ -3,41 +3,50 @@ import java.util.Queue;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
+import java.net.URL;
+import java.net.MalformedURLException;
 
 public  class UrlCrawler {
 
-	private String startingUrl = null ;
+	private String startingUrl = null;
+	// queue which includes the urls to be examined
+	Queue <String>urlQueue = new LinkedList<String>();
+	// set of already examined web pages
+	Set <String> visitedUrl = new HashSet<String>();
+	HashMap<String,String> urlsMap = new HashMap<String,String>();
 
 	public UrlCrawler(String urlName){
-
 		startingUrl = urlName;
+		urlQueue.add(startingUrl);
 	}
 
 	public HashMap<String,String> crawl() {
 
-		// queue which includes the urls to be examined\
-		Queue <String>urlQueue = new LinkedList<String>();
-		urlQueue.add(startingUrl);
+		while (visitedUrl.size() < 10 && (!urlQueue.isEmpty())) {
+			String top = urlQueue.remove();
+			if (visitedUrl.contains(top)) {
+				continue;
+			}
+			URL presentURL;
+			try {
+				presentURL = new URL(top);
+			} catch (MalformedURLException e) {
+				System.out.println("1 exception caught and handled in UrlCrawler class");
+				continue;
+			}
 
-		// set of already examined web pages
-		Set <String> visitedUrl = new HashSet<String>();
+			LinksExtractor le = new LinksExtractor(presentURL);
+			DownloadHtml dh = new DownloadHtml(presentURL);
 
-		HashMap<String,String> urlsMap = new HashMap<String,String>();
+			HashSet<String> leResults = le.getLinks();
+			String dhResults = dh.parseAndSaveHtml();
 
-		while (visitedUrl.size() < 10) {
-
-			//the connection with other parts are missing
-
+			if (leResults != null && dhResults != null) {
+				visitedUrl.add(top);
+				urlQueue.addAll(leResults);
+				urlsMap.put(top, dhResults);
+			}
 		}
-
 		return urlsMap;
-
 	}
 }
-
-
-
-
-
-
-
